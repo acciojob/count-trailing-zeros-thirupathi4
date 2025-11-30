05098308d714e48b1c22a1526a19319345f523de
Thirupathi Rao <162027483+thirupathi4@users.noreply.github.com>
2025-11-30 12:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let res=1;
+	for(let i=1;i<=n;i++){
+		res*=i;
+	}
+	let Sres=res.toString();
+	let cnt=0;
+	for(let i=Sres.length-1;i>=0;i--){
+		if(Sres[i]==0){
+			cnt++;
+		}
+		else{
+			break;
+		}
+	}
+  return cnt;
 }
 
 const input = prompt("Enter a number");
